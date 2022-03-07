4d9aa77b12f508aa60602940460f51087b096cc3
VitaliyPritula <54078944+VitaliyPritula@users.noreply.github.com>
2022-03-07 15:44:09 +0200

    верстка сайта Лого

--- #src/js/burger.js
+++
@@ -1,9 +0,0 @@
-// Меню бургер
-const iconMenu = document.querySelector(".icon-menu");
-if (iconMenu) {
-  const menuBody = document.querySelector(".menu__body");
-  iconMenu.addEventListener("click", function (e) {
-    iconMenu.classList.toggle("_active");
-    menuBody.classList.toggle("_active");
-  });
-}
